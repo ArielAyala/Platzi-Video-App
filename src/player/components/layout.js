3dc6356e772ecf6170ec8a6416fda3b898da4421
Ariel <arielayalamail@gmail.com>
2018-11-28 08:05:57 -0300

    Agregando elementos al reproductor de video

--- src/player/components/layout.js
+++ src/player/components/layout.js
@@ -11,6 +11,13 @@ function Layout(props) {
             <View style={styles.video}>
                 {props.video}
             </View>
+            <View style={styles.overlay} >
+                {
+                    props.loading &&
+                    props.loader
+                }
+            </View>
+            {props.controls}
         </View>
     )
 }
@@ -26,6 +33,15 @@ const styles = StyleSheet.create({
         right: 0,
         bottom: 0,
         backgroundColor: 'black',
+    },
+    overlay: {
+        position: 'absolute',
+        left: 0,
+        top: 0,
+        bottom: 0,
+        right: 0,
+        justifyContent: 'center',
+        alignItems: 'center',
     }
 })
 
--- src/player/containers/player.js
+++ src/player/containers/player.js
@@ -1,20 +1,61 @@
 import React, { Component } from 'react';
 import Video from 'react-native-video';
 import {
-    StyleSheet
+    StyleSheet,
+    ActivityIndicator,
+    Text
 } from 'react-native';
 import Layout from '../components/layout';
+import ControlLayout from '../components/control-layout';
+import PlayPause from '../components/play-pause';
 
 class Player extends Component {
+    state = {
+        loading: true,
+        paused: false
+    }
+    onBuffer = ({ isBuffering }) => {
+        this.setState({
+            loading: isBuffering
+        })
+    }
+    onLoad = () => {
+        this.setState({
+            loading: false
+        })
+    }
+    playPause = () => {
+        this.setState({
+            paused: !this.state.paused
+        })
+    }
     render() {
         return (
             <Layout
+                loading={this.state.loading}
                 video={
                     <Video
                         source={{ uri: 'https://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4' }}
                         style={styles.video}
                         resizeMode="contain"
+                        onBuffer={this.onBuffer}
+                        onLoad={this.onLoad}
+                        paused={this.state.paused}
                     />
+                }
+                loader={
+                    <ActivityIndicator color="red" />
+                }
+                controls={
+                    <ControlLayout>
+                        <PlayPause
+                            onPress={this.playPause}
+                            pause={this.state.paused}
+                        />
+                        <Text>progres bar</Text>
+                        <Text>tieme left</Text>
+                        <Text>fullscreen</Text>
+                    </ControlLayout>
                 }
             />
         )
